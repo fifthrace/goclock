df2f5acb18750e2f31bbec500354e30cee2951d2
Robbie Baxter <robbie.baxter@internal.salesforce.com>
2020-01-14 08:44:09 -0500

    get a digit ready

---
+++ src/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Let's begin!\n")
+
+	zero := [5][3]string {
+		[3]string = {"█","█","█"},
+		[3]string = {"█","░","█"},
+		[3]string = {"█","░","█"},
+		[3]string = {"█","░","█"},
+		[3]string = {"█","█","█"},
+	}
+
+}
