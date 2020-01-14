7f0c9c011da4d40d99f19a5374d88d0ca54e0ed0
Robbie Baxter <fifth.race@gmail.com>
2020-01-14 14:07:35 -0500

    Making some progres, but there be bugs!

--- src/main.go
+++ src/main.go
@@ -4,10 +4,53 @@ import (
 	"fmt"
 
 	clockdigits "github.com/fifthrace/goclock/pkg/clockdigits"
+	"github.com/inancgumus/screen"
 )
 
 func main() {
+	newClock := [8]byte{1, 0, clockdigits.Colon, 2, 3, clockdigits.Colon, 5, 9}
 
-	fmt.Printf("%+v", clockdigits.GetDigit(1))
+	screen.Clear()
 
+	screen.MoveTopLeft()
+	printDigitsLefttoRight(newClock)
+}
+
+func printSingleDigit(n byte) {
+	d := clockdigits.GetDigit(n)
+
+	for _, b := range d {
+		for _, c := range b {
+			if c {
+				fmt.Print("█")
+			} else {
+				fmt.Print("░")
+			}
+		}
+		fmt.Println("")
+	}
+}
+
+func printDigitsLefttoRight(d [8]byte) {
+	var clock [8]clockdigits.Digit
+
+	//build my clock array
+	for i, digit := range d {
+		clock[i] = clockdigits.GetDigit(digit)
+	}
+
+	//need to print each line from left to right, so we'll loop over lines then the
+	//digits representation per line
+	for linenum := range clock[0] {
+		for _, digit := range clock[linenum] {
+			for _, pixel := range digit {
+				if pixel {
+					fmt.Print("█")
+				} else {
+					fmt.Print("░")
+				}
+			}
+		}
+		fmt.Println()
+	}
 }
